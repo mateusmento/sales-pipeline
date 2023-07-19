28b3326e682bd1cd4667ce60515fe6df35a0ce43
Mateus Sarmento <omateusmento@gmail.com>
2023-07-19 09:06:26 -0300

    Include TopbarHeader in App

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import TopbarHeader from '@/lib/components/TopbarHeader.vue';
 import { RouterView } from 'vue-router';
 </script>
 
 <template>
+  <TopbarHeader />
   <RouterView />
 </template>
 
