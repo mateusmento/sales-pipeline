4c397f0a96a47e99b9e3a724f6bc9c4a358442f6
Mateus Sarmento <omateusmento@gmail.com>
2023-07-19 08:38:10 -0300

    Add IconChecked

---
+++ src/lib/icons/IconChecked.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex">
+    <svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M8.75 1L3.25 6.5L0.75 4"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </div>
+</template>
